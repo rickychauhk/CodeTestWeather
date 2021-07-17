ef247a81d24426effba1360694da92f1c44b02a8
RickyChau <ricky.chau@nextdigital.com.hk>
2021-07-17 22:25:25 +0800

    create test cases for api and view model

--- CodeTestWeather/CodeTestWeatherTests/CodeTestWeatherTests.swift
+++ CodeTestWeather/CodeTestWeatherTests/CodeTestWeatherTests.swift
@@ -9,7 +9,50 @@ import XCTest
 @testable import CodeTestWeather
 
 class CodeTestWeatherTests: XCTestCase {
-
+    var viewModel: Weather!
+    
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        viewModel = nil
+        super.tearDown()
+    }
+    
+    func testJSONDecoding() {
+        
+        // Convert restaurant.json to Data
+        
+        let testBundle = Bundle(for: type(of: self))
+        let path = testBundle.path(forResource: "restaurants", ofType: "json")
+        guard let data = try? Data(contentsOf: URL(fileURLWithPath: path!), options: .alwaysMapped) else {
+            fatalError("Data is nil")
+        }
+        
+        let resource = try! JSONDecoder().decode(Weather.self, from: data)
+    
+        XCTAssertEqual(resource.name, "HongKong")
+    }
+    
+    func testResourceDetailViewModel() {
+        let testBundle = Bundle(for: type(of: self))
+        let path = testBundle.path(forResource: "restaurants", ofType: "json")
+        guard let data = try? Data(contentsOf: URL(fileURLWithPath: path!), options: .alwaysMapped) else {
+            fatalError("Data is nil")
+        }
+        
+        let resource = try! JSONDecoder().decode(Weather.self, from: data)
+        
+//        guard let firResource = resource else {
+//            fatalError("First Resource is nil")
+//        }
+        
+        viewModel = Weather(value: resource)
+        
+        XCTAssertEqual(viewModel.name, "HongKong")
+    }
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
